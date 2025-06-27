12de577fe220a6af1a952739af1f1821100113f6
evymii <n.munkhpurev@gmail.com>
2025-06-27 17:07:55 +0800

    backend issue solved

--- backend2/src/index.ts
+++ backend2/src/index.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
-import foodsRouter from "./routes/food.route.js";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+
+import foodsRouter from "./routes/food.route.js";
 import orderRouter from "./routes/order.route.js";
 import categRouter from "./routes/category.route.js";
 import authRouter from "./routes/authentic.route.js";
 import userRouter from "./routes/user.route.js";
-import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -21,10 +22,6 @@ server.use("/foodorder", orderRouter);
 server.use("/foodcategory", categRouter);
 server.use("/auth", authRouter);
 server.use("/user", userRouter);
-
-// server.get("/", (_request, response) => {
-//   response.send("modelsw2");
-// });
 
 server.listen(port, () => {
   console.log(`Server aslaa http://localhost:${port}`);
